b38d6099d6fe67fb7cd91ff35849645ec2fd4179
JungUiJin <50124987+JungUiJin@users.noreply.github.com>
2025-06-12 02:27:14 +0900

    Update ResultPage.tsx

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,5 +1,5 @@
 // ResultPage.tsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import styles from '../styles/ResultPage.module.css';
 import ResultDetailSlideUp from '../components/ResultDetailSlideUp';
